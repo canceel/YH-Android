a04ae3c308b7b5a92c1a0c9a61d1541081e55f09
Liurl21 <Liurl21@outlook.com>
2017-04-23 00:45:18 +0800

    crontab task

--- git.sh
+++ git.sh
@@ -1,3 +1,17 @@
-git add ./
-git commit -m'crontab task'
-git push origin developer
+gitPush() {
+git symbolic-ref --short -q HEAD
+
+  br=`git branch | grep "*"`
+
+
+if [ "${br/* /}" = "developer" ] ; then
+    git add ./
+    git commit -m'crontab task'
+    git push origin developer
+
+else
+    echo "no"
+fi
+}
+gitPush
+

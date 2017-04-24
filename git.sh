de82c0bbdf5c068ecd99eae54b83c8faaf1bd7ab
Liurl21 <Liurl21@outlook.com>
2017-04-24 09:52:47 +0800

    crontab task ${timestamp}

--- git.sh
+++ git.sh
@@ -1,11 +1,11 @@
 gitPush() {
 
   br=`git symbolic-ref --short -q HEAD`
-
+  timestamp=$(date "+%Y-%m-%d %H:%M:%S")
 
 if [ "$br" = "developer" ] ; then
     git add ./
-    git commit -m'crontab task'
+    git commit -m'crontab task ${timestamp}'
     git push origin developer
 
 else

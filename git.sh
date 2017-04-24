fd7301a2d02473b1c7965ef7588195ea871f85f3
Liurl21 <Liurl21@outlook.com>
2017-04-24 09:56:44 +0800

    ${commit_message}

--- git.sh
+++ git.sh
@@ -1,12 +1,15 @@
 gitPush() {
 
   br=`git symbolic-ref --short -q HEAD`
-  timestamp=$(date "+%Y-%m-%d %H:%M:%S")
 
 if [ "$br" = "developer" ] ; then
-    git add ./
-    git commit -m'crontab task ${timestamp}'
-    git push origin developer
+
+  timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+  commit_message=$("crontab task ${timestamp}")
+
+  git add ./
+  git commit -m'${commit_message}'
+  git push origin developer
 
 else
     echo "current branch is not developer"

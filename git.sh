9d3a9b53ee9b5377d30caf2219203a46394390bb
Liurl21 <Liurl21@outlook.com>
2017-04-24 09:49:33 +0800

    crontab task

--- git.sh
+++ git.sh
@@ -1,16 +1,15 @@
 gitPush() {
-git symbolic-ref --short -q HEAD
 
-  br=`git branch | grep "*"`
+  br=`git symbolic-ref --short -q HEAD`
 
 
-if [ "${br/* /}" = "developer" ] ; then
+if [ "$br" = "developer" ] ; then
     git add ./
     git commit -m'crontab task'
     git push origin developer
 
 else
-    echo "no"
+    echo "current branch is not developer"
 fi
 }
 gitPush

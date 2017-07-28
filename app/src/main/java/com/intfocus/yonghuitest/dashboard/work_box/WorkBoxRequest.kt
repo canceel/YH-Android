15a16b99d7845a0ef4350415e3b1314686387728
Liurl21 <Liurl21@outlook.com>
2017-07-28 14:03:54 +0800

    for checkout

--- app/src/main/java/com/intfocus/yonghuitest/dashboard/work_box/WorkBoxRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.intfocus.yonghuitest.dashboard.work_box
-
-/**
- * Created by liuruilin on 2017/7/28.
- */
-class WorkBoxRequest(isSuccess: Boolean, stateCode: Int) {
-    var workBoxDatas: WorkBoxBean? = null
-}
